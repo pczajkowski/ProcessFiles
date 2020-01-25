f3a74370129366cd14c758f55ff3e2bb0e216800
Piotr Czajkowski <pczajkowski@gmail.com>
2020-01-25 11:58:44 +0100

    First version

---
+++ ProcessFiles/ProcessFiles.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ProcessFiles
+{
+    public static class ProcessFiles
+    {
+        private static List<string> _errors;
+        private static void ProcessDir(string path, string fileExtension, Action<string> callback, bool recursive = false)
+        {
+            var searchOption = SearchOption.TopDirectoryOnly;
+            if (recursive)
+                searchOption = SearchOption.AllDirectories;
+
+            var files = Directory.GetFiles(path, $"*.{fileExtension}", searchOption);
+            if (!files.Any())
+            {
+                _errors.Add($"There are no {fileExtension} files in {path}!");
+                return;
+            }
+
+            foreach (var file in files)
+                callback(file);
+        }
+
+        private static bool CheckForFile(string argument, string fileExtension, Action<string> callback)
+        {
+            if (!File.Exists(argument))
+                return false;
+
+            var extension = Path.GetExtension(argument);
+            if (string.IsNullOrWhiteSpace(extension))
+            {
+                _errors.Add($"Can't establish extension of {argument}!");
+                return false;
+            }
+
+            if (!extension.Equals($".{fileExtension}"))
+            {
+                _errors.Add($"Extension of {argument} doesn't match {fileExtension}!");
+                return false;
+            }
+
+            callback(argument);
+            return true;
+        }
+
+        public static IEnumerable<string> Process(IEnumerable<string> arguments, string fileExtension, Action<string> callback, bool recursive = false)
+        {
+            _errors = new List<string>();
+
+            foreach (var argument in arguments)
+            {
+                if (CheckForFile(argument, fileExtension, callback))
+                    continue;
+
+                if (!Directory.Exists(argument))
+                {
+                    _errors.Add($"{argument} is neither file nor directory!");
+                    continue;
+                }
+
+                ProcessDir(argument, fileExtension, callback, recursive);
+            }
+
+            return _errors;
+        }
+    }
+}
---
+++ ProcessFilesTests/ProcessFilesTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace ProcessFilesTests
+{
+    public class ProcessFilesTests
+    {
+        [Fact]
+        public void ProcessFolderTest()
+        {
+            const string expected = "test1.txt";
+            var result = string.Empty;
+
+            void Callback(string value)
+            {
+               result = Path.GetFileName(value);
+            }
+
+            var errors = ProcessFiles.ProcessFiles.Process(new [] {"./testFiles"}, "txt", Callback);
+            Assert.Empty(errors);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ProcessFolderRecursiveTest()
+        {
+            var expected = new[]
+            {
+                "test1.txt",
+                "test2.txt"
+            };
+
+            var result = new List<string>();
+            void Callback(string value)
+            {
+                result.Add(Path.GetFileName(value));
+            }
+
+            var errors = ProcessFiles.ProcessFiles.Process(new[] { "./testFiles" }, "txt", Callback, true);
+            Assert.Empty(errors);
+
+            bool CheckResult()
+            {
+                foreach (var item in result)
+                {
+                    if (!expected.Contains(item))
+                        return false;
+                }
+
+                return true;
+            }
+
+            Assert.True(CheckResult());
+        }
+
+        [Fact]
+        public void ProcessFileTest()
+        {
+            var expected = @"./testFiles/test1.txt";
+            var result = string.Empty;
+
+            void Callback(string value)
+            {
+                result = value;
+            }
+
+            var errors = ProcessFiles.ProcessFiles.Process(new[] { "./testFiles/test1.txt" }, "txt", Callback);
+            Assert.Empty(errors);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ProcessFileNotExistTest()
+        {
+            var expected = @"./testFiles/test1.txt";
+            var result = string.Empty;
+
+            void Callback(string value)
+            {
+                result = value;
+            }
+
+            var errors = ProcessFiles.ProcessFiles.Process(new[] { "./testFiles/test.txt" }, "txt", Callback);
+            Assert.NotEmpty(errors);
+
+            Assert.NotEqual(expected, result);
+        }
+
+        [Fact]
+        public void ProcessFileNotMatchExtensionTest()
+        {
+            var expected = @"./testFiles/test1.txt";
+            var result = string.Empty;
+
+            void Callback(string value)
+            {
+                result = value;
+            }
+
+            var errors = ProcessFiles.ProcessFiles.Process(new[] { "./testFiles/test1.txt" }, "abc", Callback);
+            Assert.NotEmpty(errors);
+
+            Assert.NotEqual(expected, result);
+        }
+    }
+}
