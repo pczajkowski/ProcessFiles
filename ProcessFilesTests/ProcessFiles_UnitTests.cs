c52f033ea1b0c3ea385c2f09c901704454bde810
Piotr Czajkowski <pczajkowski@gmail.com>
2024-10-25 14:06:41 +0200

    Added ProcessFileNotMatchExtensionTest

--- ProcessFilesTests/ProcessFiles_UnitTests.cs
+++ ProcessFilesTests/ProcessFiles_UnitTests.cs
@@ -1,6 +1,7 @@
 ﻿using NSubstitute;
 using ProcessFiles.Interfaces;
 using Xunit;
+using Xunit.Sdk;
 
 namespace ProcessFilesTests
 {
@@ -24,6 +25,28 @@ namespace ProcessFilesTests
             Assert.Empty(result);
 
             fakeFileSystem.File.Received().Exists(Arg.Any<string>());
+        }
+
+        [Fact]
+        public void ProcessFileNotMatchExtensionTest()
+        {
+            var result = string.Empty;
+            void TestAction(string value)
+            {
+                result = value;
+            }
+
+            var fakeFileSystem = Substitute.For<IFileSystem>();
+            fakeFileSystem.File.Exists(Arg.Any<string>()).Returns(true);
+            fakeFileSystem.Path.GetExtension(Arg.Any<string>()).Returns(string.Empty);
+
+            var test = new ProcessFiles.ProcessFiles(fakeFileSystem);
+            var errors = test.Process(["imaginaryNoExtension"], "abc", TestAction);
+            Assert.NotEmpty(errors);
+            Assert.Empty(result);
+
+            fakeFileSystem.File.Received().Exists(Arg.Any<string>());
+            fakeFileSystem.Path.Received().GetExtension(Arg.Any<string>());
         }
     }
 }
