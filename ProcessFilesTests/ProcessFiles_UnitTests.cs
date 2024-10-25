7d26f74ca31ba9020115f3001d88db87051e2cde
Piotr Czajkowski <pczajkowski@gmail.com>
2024-10-25 14:26:20 +0200

    More tests

--- ProcessFilesTests/ProcessFiles_UnitTests.cs
+++ ProcessFilesTests/ProcessFiles_UnitTests.cs
@@ -28,7 +28,7 @@ namespace ProcessFilesTests
         }
 
         [Fact]
-        public void ProcessFileNotMatchExtensionTest()
+        public void ProcessFileNoExtensionTest()
         {
             var result = string.Empty;
             void TestAction(string value)
@@ -42,6 +42,28 @@ namespace ProcessFilesTests
 
             var test = new ProcessFiles.ProcessFiles(fakeFileSystem);
             var errors = test.Process(["imaginaryNoExtension"], "abc", TestAction);
+            Assert.NotEmpty(errors);
+            Assert.Empty(result);
+
+            fakeFileSystem.File.Received().Exists(Arg.Any<string>());
+            fakeFileSystem.Path.Received().GetExtension(Arg.Any<string>());
+        }
+
+        [Fact]
+        public void ProcessFileNotMatchExtensionTest()
+        {
+            var result = string.Empty;
+            void TestAction(string value)
+            {
+                result = value;
+            }
+
+            var fakeFileSystem = Substitute.For<IFileSystem>();
+            fakeFileSystem.File.Exists(Arg.Any<string>()).Returns(true);
+            fakeFileSystem.Path.GetExtension(Arg.Any<string>()).Returns("def");
+
+            var test = new ProcessFiles.ProcessFiles(fakeFileSystem);
+            var errors = test.Process(["imaginaryFile"], "abc", TestAction);
             Assert.NotEmpty(errors);
             Assert.Empty(result);
 
